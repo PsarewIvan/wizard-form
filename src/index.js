b201e70d5c912784eec711409d1a85345e29e0e0
Ivan Psarew <psarew.ivan@yandex.ru>
2021-05-12 21:15:05 +0300

    refactor

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import 'normalize.css';
 
@@ -10,11 +9,9 @@ import './index.scss';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </Router>
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
